84c42035d4ee1f683056e3000ff6a0ef1aa6093b
Kim Lan Phan Hoang <pyphilia@gmail.com>
2022-08-02 14:34:31 +0200

    9/sdk (#10)
    * build: update dependencies
    
    * refactor: rename commitlint config
    
    * refactor: use graasp sdk

--- src/util/aux.ts
+++ src/util/aux.ts
@@ -1,15 +1,18 @@
 import bcrypt from 'bcrypt';
+import cryptoRandomString from 'crypto-random-string';
+
 import { ItemLoginSchema } from '../interfaces/item-login';
 
-const randomHexOf4 = () => (Math.random() * (1 << 16) | 0).toString(16).padStart(4, '0');
+const randomHexOf4 = () => cryptoRandomString({ length: 4, type: 'hex' });
 const saltRounds = 10;
 
-export const encryptPassword =
-  async (password: string): Promise<string> => bcrypt.hash(password, saltRounds);
+export const encryptPassword = async (password: string): Promise<string> =>
+  bcrypt.hash(password, saltRounds);
 
-export const validatePassword =
-  async (plainTextPassword: string, passwordHash: string): Promise<boolean> =>
-    bcrypt.compare(plainTextPassword, passwordHash);
+export const validatePassword = async (
+  plainTextPassword: string,
+  passwordHash: string,
+): Promise<boolean> => bcrypt.compare(plainTextPassword, passwordHash);
 
 export const loginSchemaRequiresPassword = (loginSchema: ItemLoginSchema): boolean =>
   loginSchema === ItemLoginSchema.UsernameAndPassword ||
--- src/util/graasp-item-login-error.ts
+++ src/util/graasp-item-login-error.ts
@@ -1,79 +1,130 @@
-import { GraaspErrorDetails, GraaspError } from 'graasp';
+import { StatusCodes } from 'http-status-codes';
 
-export class GraaspItemLoginError implements GraaspError {
-  name: string;
-  code: string
-  message: string;
-  statusCode?: number;
-  data?: unknown;
-  origin: 'plugin' | string;
+import { ErrorFactory } from '@graasp/sdk';
 
-  constructor({ code, statusCode, message }: GraaspErrorDetails, data?: unknown) {
-    this.name = code;
-    this.code = code;
-    this.message = message;
-    this.statusCode = statusCode;
-    this.data = data;
-    this.origin = 'plugin';
-  }
-}
+import { PLUGIN_NAME } from './constants';
+
+export const GraaspItemLoginError = ErrorFactory(PLUGIN_NAME);
 
 export class ItemNotFound extends GraaspItemLoginError {
   constructor(data?: unknown) {
-    super({ code: 'GILERR001', statusCode: 404, message: 'Item not found' }, data);
+    super(
+      { code: 'GILERR001', statusCode: StatusCodes.NOT_FOUND, message: 'Item not found' },
+      data,
+    );
   }
 }
 
 export class MemberIdentifierNotFound extends GraaspItemLoginError {
   constructor(data?: unknown) {
-    super({ code: 'GILERR002', statusCode: 404, message: 'Member identifier not found' }, data);
+    super(
+      {
+        code: 'GILERR002',
+        statusCode: StatusCodes.NOT_FOUND,
+        message: 'Member identifier not found',
+      },
+      data,
+    );
   }
 }
 
 export class InvalidMember extends GraaspItemLoginError {
   constructor(data?: unknown) {
-    super({ code: 'GILERR003', statusCode: 404, message: 'This member cannot be used to login to an item' }, data);
+    super(
+      {
+        code: 'GILERR003',
+        statusCode: StatusCodes.NOT_FOUND,
+        message: 'This member cannot be used to login to an item',
+      },
+      data,
+    );
   }
 }
 
 export class MissingItemLoginSchema extends GraaspItemLoginError {
   constructor(data?: unknown) {
-    super({ code: 'GILERR004', statusCode: 500, message: 'Missing login schema' }, data);
+    super(
+      {
+        code: 'GILERR004',
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        message: 'Missing login schema',
+      },
+      data,
+    );
   }
 }
 
 export class MissingItemLoginTag extends GraaspItemLoginError {
   constructor(data?: unknown) {
-    super({ code: 'GILERR005', statusCode: 400, message: 'Item does not possess the required tag' }, data);
+    super(
+      {
+        code: 'GILERR005',
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'Item does not possess the required tag',
+      },
+      data,
+    );
   }
 }
 
 export class ValidMemberSession extends GraaspItemLoginError {
   constructor(data?: unknown) {
-    super({ code: 'GILERR006', statusCode: 400, message: 'Member with valid session trying to (re)login' }, data);
+    super(
+      {
+        code: 'GILERR006',
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'Member with valid session trying to (re)login',
+      },
+      data,
+    );
   }
 }
 
 export class InvalidCredentials extends GraaspItemLoginError {
   constructor(data?: unknown) {
-    super({ code: 'GILERR007', statusCode: 401, message: 'Provided credentials don\'t match member\'s' }, data);
+    super(
+      {
+        code: 'GILERR007',
+        statusCode: StatusCodes.UNAUTHORIZED,
+        // eslint-disable-next-line quotes
+        message: "Provided credentials don't match member's",
+      },
+      data,
+    );
   }
 }
 
 export class MissingCredentialsForLoginSchema extends GraaspItemLoginError {
   constructor(data?: unknown) {
-    super({ code: 'GILERR008', statusCode: 400, message: 'Missing credentials for set login schema' }, data);
+    super(
+      {
+        code: 'GILERR008',
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'Missing credentials for set login schema',
+      },
+      data,
+    );
   }
 }
 
 export class UnnecessaryCredentialsForLoginSchema extends GraaspItemLoginError {
   constructor(data?: unknown) {
-    super({ code: 'GILERR008', statusCode: 400, message: 'Unnecessary credentials for set login schema' }, data);
+    super(
+      {
+        code: 'GILERR008',
+        statusCode: StatusCodes.BAD_REQUEST,
+        message: 'Unnecessary credentials for set login schema',
+      },
+      data,
+    );
   }
 }
 
 export class MemberCannotAdminItem extends GraaspItemLoginError {
   constructor(data?: unknown) {
-    super({ code: 'GILERR009', statusCode: 403, message: 'Member cannot admin item' }, data);
+    super(
+      { code: 'GILERR009', statusCode: StatusCodes.FORBIDDEN, message: 'Member cannot admin item' },
+      data,
+    );
   }
 }
